772f3ab3d2e58e0538b2ccfa52582753980aed11
APCode <josealpama98@hotmail.com>
2023-11-09 20:06:53 -0500

    Update Next.js starter template with new dashboard pages and layout

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Akaya_Telivigala, Yuji_Hentaigana_Akebono, Lusitana } from 'next/font/google'
+
+export const akaya = Akaya_Telivigala({
+    weight:'400',
+    subsets: ['latin']
+});
+export const yuji = Yuji_Hentaigana_Akebono({
+    weight:'400',
+    subsets: ['latin']
+});
+export const lusitana = Lusitana({
+    weight:['400', '700'],
+    subsets: ['latin']
+});
