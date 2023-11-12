c893f676e5025c321223d2003c5bc4d7519b2315
APCode <josealpama98@hotmail.com>
2023-11-12 17:59:35 -0500

    Refactor dashboard page and related components fot Streaming Ch9

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,10 +1,10 @@
-import { Akaya_Telivigala, Yuji_Hentaigana_Akebono, Lusitana } from 'next/font/google'
+import { Akaya_Telivigala, Yuji_Hentaigana_Akari, Lusitana } from 'next/font/google'
 
 export const akaya = Akaya_Telivigala({
     weight:'400',
     subsets: ['latin']
 });
-export const yuji = Yuji_Hentaigana_Akebono({
+export const yuji = Yuji_Hentaigana_Akari({
     weight:'400',
     subsets: ['latin']
 });
